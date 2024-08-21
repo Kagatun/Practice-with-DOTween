1c0030bd966917cba82f2238cca60065a90cfe97
Kagatun <KAGATUN777@MAIL.RU>
2024-08-21 10:56:35 +0300

    2
    2

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,15 +4,13 @@ using DG.Tweening;
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private LoopType _loopType;
-    [SerializeField] private float _rotationX;
-    [SerializeField] private float _rotationY;
-    [SerializeField] private float _rotationZ;
+    [SerializeField] private Vector3 _rotation;
     [SerializeField] private float _duration;
     [SerializeField] private int _repeat;
 
     private void Start()
     {
-        transform.DORotate(new Vector3(_rotationX, _rotationY, _rotationZ), _duration, RotateMode.FastBeyond360)
+        transform.DORotate(_rotation, _duration, RotateMode.FastBeyond360)
             .SetLoops(_repeat, _loopType).SetEase(Ease.Linear);
     }
 }
